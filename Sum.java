3566cdb72abe37f8e5bfac1bf7a8b55bd3470e0a
Pankaj Bhagat <Pankajbhagat8888@gmail.com>
2023-07-31 00:49:29 +0530

    main code

--- Sum.java
+++ Sum.java
@@ -2,7 +2,7 @@ class Sum{
 
     public static void main(String[] args) {
   
-        System.out.println("Kal exam hai aur mereko tension hai");
+        System.out.println("Kal exam hai aur mereko tension bahut hai");
     
 }
 
